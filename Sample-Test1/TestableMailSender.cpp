378da97022a687bd677efd7158aea442b72463f8
donghoonyeo <www1w1w11@gmail.com>
2024-05-31 17:07:55 +0900

    Restaurant Booking initial KATA

---
+++ Sample-Test1/TestableMailSender.cpp
@@ -0,0 +1,15 @@
+#include "../Project19/MailSender.cpp"
+
+class TestableMailSender : public MailSender {
+public:
+	void sendMail(Schedule* schedule) override {
+		countSendMailMethodIsCalled++;
+	}
+
+	int getCountSendMailMethodIsCalled() {
+		return countSendMailMethodIsCalled;
+	}
+
+private:
+	int countSendMailMethodIsCalled = 0;
+};
